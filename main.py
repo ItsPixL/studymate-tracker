c147df1a4cac996919498abe83efd50655c249bb
Neer Sheth <sheth.neer27@gmail.com>
2025-06-20 16:50:00 +1000

    Added separate general functions for adding and removing subjects. Wrote a function to input a subject to add (using the general function created before), and another to remove a subject (again using general function). Also added a QOL improvement to the logging mechanism where if a subject is not in the user's subject list, it will automatically add it to the list if the user accepts, instead of having to make the user manually add it then log.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tracker import add_subject, log_session
+from tracker import input_new_subject, log_session, input_subject_to_remove
 
 # Startup CLI Menu
 def main():
@@ -9,7 +9,8 @@ def main():
         print("2. Log session")
         print("3. View all sessions")
         print("4. View total time")
-        print("5. Exit")
+        print("5: Remove Subject")
+        print("6. Exit")
         print()
 
         # Get user input
@@ -19,7 +20,7 @@ def main():
         # Run something based on user input
         match choice:
             case "1":
-                add_subject()
+                input_new_subject()
             case "2":
                 log_session()
             case "3":
@@ -29,6 +30,8 @@ def main():
                 # call view
                 print(4)
             case "5":
+                input_subject_to_remove()
+            case "6":
                 break
             case _:
                 print("Invalid input")
--- tracker.py
+++ tracker.py
@@ -23,12 +23,8 @@ def write_file(filename, data):
     with open(filename, 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
-
-# ------------------------------------------------------------
-# ------- Function for adding subject to subjects file -------
-# ------------------------------------------------------------
-def add_subject():
-    new_subject = input("Subject Name: ")
+# Add Subject
+def add_subject(new_subject):
     data = read_file(subjects)
     if new_subject in data:
         print("Subject already in list. \n")
@@ -36,13 +32,77 @@ def add_subject():
     else:
         data.append(new_subject)
     write_file(subjects, data)
+    print("\nSuccessfully added subject! \n")
+
+# Remove Subject
+def remove_subject(subject):
+    # Remove subject from subjects file
+    data = read_file(subjects)
+    if subject in data:
+        data.remove(subject)
+        write_file(subjects, data)
+    else:
+        print("Subject not found in list. \n")
+        exit
+
+    # Remove any sessions associated with that subject
+    new_logs_data = []
+    logs_data = read_file(sessions)
+    for session in logs_data:
+        if session["subject"] == subject:
+            print("Removed one log:", session)
+        else:
+            new_logs_data.append(session)
+    write_file(sessions, new_logs_data)
+
+
+# ------------------------------------------------------------
+# ------- Function for adding subject to subjects file -------
+# ------------------------------------------------------------
+def input_new_subject():
+    new_subject = input("Subject Name: ").strip()
+    add_subject(new_subject)
+
+# ------------------------------------------------------------
+# ----- Function for removing subject from subjects file -----
+# ------------------------------------------------------------
+def input_subject_to_remove():
+    removal_subject = input("Subject to remove: ").strip()
+    warning_res = input("WARNING: Are you sure you want to permanently remove this subject? This will delete any sessions associated with that subject. (y/n) ").strip().lower()
+    if warning_res in ("y", "yes"):
+        remove_subject(removal_subject)
+    elif warning_res in ("n", "no"):
+        print("Stopping Removal\n")
+        exit
+
 
 # ------------------------------------------------------------
 # ------ Function for logging sessions to sessions file ------
 # ------------------------------------------------------------
 def log_session():
-    subject = input("Subject: ")
-    duration = input("Duration: ")
+    subject = input("Subject: ").strip()
+
+    subjects_list = read_file(subjects)
+    if subject not in subjects_list:
+        choice = input("Subject not found! Would you like to add it to your subject's list? (y/n) ").strip().lower()
+        if choice in ("y", "yes"):
+            add_subject(subject)
+        elif choice in ("n", "no"):
+            print()
+            return
+        else:
+            print("Invalid input \n")
+            return
+
+    while True:
+        try:
+            duration = int(input("Duration (minutes): "))
+            if duration <= 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid input. Please enter a positive whole number \n")
+
     today = date.today().strftime("%Y-%m-%d")
 
     new_session = {
@@ -54,3 +114,4 @@ def log_session():
     data = read_file(sessions)
     data.append(new_session)
     write_file(sessions, data)
+    print("\nSuccessfully logged session! \n")
