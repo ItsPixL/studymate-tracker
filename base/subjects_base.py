5cb52157017bc175d9f6fbe535aa39069fb17c65
Neer Sheth <sheth.neer27@gmail.com>
2025-06-30 19:35:47 +1000

    Pretty big commit, but I started work on the frontend website. This is done with Vite React using TypeScript.

--- base/subjects_base.py
+++
@@ -1,6 +0,0 @@
-from .utils import SUBJECTS, read_file
-
-def check_if_subject_exists(subject):
-    subjects_list = read_file(SUBJECTS)
-    if subject not in subjects_list: return False
-    else: return True
