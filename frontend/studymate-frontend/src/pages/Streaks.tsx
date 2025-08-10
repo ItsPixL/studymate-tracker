8edd2f51da389beacb4e118c8e9a80c8f29e3776
Neer Sheth <sheth.neer27@gmail.com>
2025-08-10 11:54:57 +1000

    Added a refresh button for stats.

--- frontend/studymate-frontend/src/pages/Streaks.tsx
+++ frontend/studymate-frontend/src/pages/Streaks.tsx
@@ -18,6 +18,16 @@ export default function Streaks() {
       .catch((err) => console.log(err.message));
   }, []);
 
+  const updateStreaks = () => {
+    fetchStreaks()
+      .then(setStreaks)
+      .catch((err) => console.log(err.message));
+  };
+
+  useEffect(() => {
+    updateStreaks();
+  }, []);
+
   const [current, longest] = streaks;
 
   return (
@@ -28,9 +38,28 @@ export default function Streaks() {
       animate="show"
       transition={{ when: "beforeChildren", delay: 1, staggerChildren: 0.1 }}
     >
-      <motion.span className="text-2xl font-bold" variants={itemVariants}>
-        Streaks
-      </motion.span>
+      <motion.div
+        className="flex justify-between items-center mb-4"
+        variants={itemVariants}
+      >
+        <motion.span className="text-2xl font-bold" variants={itemVariants}>
+          Streaks
+        </motion.span>
+        <motion.span
+          className="material-symbols-outlined cursor-pointer text-3xl select-none"
+          aria-label="Refresh subjects"
+          role="button"
+          initial={{ scale: 1 }}
+          whileHover={{
+            scale: 1.3,
+            transition: { type: "spring", stiffness: 300, damping: 20 },
+          }}
+          whileTap={{ scale: 0.9 }}
+          onClick={updateStreaks}
+        >
+          refresh
+        </motion.span>
+      </motion.div>
       <motion.div
         className="mt-2 backdrop-blur-lg bg-gradient-to-r from-blue-900 to-purple-900 py-2.5 px-5 text-2xl rounded-lg"
         variants={itemVariants}
