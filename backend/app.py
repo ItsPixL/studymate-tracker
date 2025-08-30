47f0d16a8c6ecf47b3623d93bc08c43044b00e27
Neer Sheth <sheth.neer27@gmail.com>
2025-08-30 16:49:28 +1000

    getting warnings from flask idk why

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
+from flask import Flask, request, jsonify, send_from_directory # type: ignore
+from flask_cors import CORS # type: ignore
 import os
 import sys
 import webbrowser
